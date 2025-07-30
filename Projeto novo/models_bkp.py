717f94ab0af4b70adfdae0a4aaef34e7d2b6a9ca
Marcos Vinícius <131191751+MarcosV04@users.noreply.github.com>
2025-07-30 00:30:01 -0300

    Delete Projeto novo directory

--- Projeto novo/models_bkp.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
-from app import db
-
-class User(db.model, UserMixin):
-    id = db.column(db.string, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
-    
